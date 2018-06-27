195129421cf15e71a064a7d7820bd93dbf52d73c
Ioannis Polyzos <i.polyzos@gmail.com>
2018-06-27 10:38:45 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,78 @@
+# Open Service Broker for Azure (OSBA) on ACS-Engine Kubernetes deployment installer
+
+[Open Service Broker for Azure (OSBA)](https://osba.sh/) on [ACS-Engine Kubernetes deployment installer](https://github.com/Azure/acs-engine) is a bash script to automate deployment of OSBA on a ACS-Engine deployed Kubernetes cluster.
+
+## Prerequisites
+
+- [Kubectl](https://github.com/kubernetes/kubectl) >= 1.9
+- [Helm](https://github.com/kubernetes/helm) v2.8.1
+
+**NOTE:**
+
+ - The script is able to manage _(i.e download and install)_ any dependencies and apropriate versions as required.
+ - Supported operating systems include [Linux](https://en.wikipedia.org/wiki/Linux) and [MacOS](https://en.wikipedia.org/wiki/Macintosh_operating_systems).
+
+## Installation
+
+Installation of the OSBA installer script only require to download in your machine.
+
+## Usage
+
+The script expects you to have logged in your Azure account and set the right subscription before run.
+
+Usage of the script is descrited help the page i.e.
+
+```
+osba.sh : OSBA Installer for ACS-Engine Kubernetes deployments.
+Usage: ./osba.sh [options]
+options:
+    -r, --remove                    Remove OSBA installation.
+    -h, --help                      Display this help message.
+```
+
+## Contributing
+
+ See the [contribution guidelines](CONTRIBUTING.md).
+
+## History
+
+2018-06-27 - Initial commit
+
+## References
+
+ - [Service Catalog](https://github.com/kubernetes-incubator/service-catalog)
+ - [Service Catalog Installation](https://github.com/kubernetes-incubator/service-catalog/blob/master/docs/install.md)
+ - [Service Catalog - Helm repository setup](https://github.com/kubernetes-incubator/service-catalog/blob/master/docs/install.md#helm-repository-setup)
+ - [Open Service Broker for Azure](https://osba.sh/)
+ - [The Open Service Broker API Server for Azure Services]( https://github.com/Azure/open-service-broker-azure)
+ - [OSBA FAQ]( https://github.com/Azure/open-service-broker-azure/blob/master/docs/faq.md)
+ - [Open Service Broker for Azure Samples](https://github.com/neilpeterson/open-service-broker-azure-samples)
+ - [OSBA Versions and Module Stability](https://github.com/Azure/open-service-broker-azure/blob/master/docs/stability.md)
+ - [Azure Modules Documentation (incl experimental)](https://github.com/Azure/open-service-broker-azure/tree/master/docs/modules)
+ - [Helm charts for use with Kubernetes service-catalog](https://github.com/Azure/helm-charts/)
+ - [Creating a new chart](https://github.com/Azure/helm-charts#creating-a-new-chart)
+ - [Open Service Broker for MongoDB on Azure provided by CosmosDB](https://github.com/neilpeterson/open-service-broker-azure-samples/tree/master/osba-cosmosdb-mongodb-sample)
+
+## License
+```
+Copyright (c) 2018 Ioannis Polyzos
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+```
---
+++ osba.sh
@@ -0,0 +1,226 @@
+#!/bin/bash
+
+# Azure OSBA Installation
+#
+# NOTE:
+#  - Supports Linux and Darwin Operating Systems
+#
+# Author: Ioannis Polyzos
+
+# Exit immediately if a pipeline returns a non-zero status.
+set -o errexit
+
+# Return value of a pipeline is the value of the last command to exit with a non-zero status or zero
+# if all commands in the pipeline exit successfully.
+set -o pipefail
+
+# Treat unset variables and parameters other than the special parameters ‘@’ or ‘*’ as an error when performing parameter expansion.
+set -o nounset
+
+# download and install Kubectl CLI tool
+function install_kubectl {
+    echo "Installing 'kubectl' CLI"
+
+    # remove current installation if exist
+    KBCTL_PATH=`command -v kubectl`
+    if [[ ${KBCTL_PATH} ]]
+    then
+      echo "exists at $KBCTL_PATH"
+    fi
+
+    # download and extract the binary for the current env
+    case "$OSTYPE" in
+        darwin*)  curl -o kubectl https://storage.googleapis.com/kubernetes-release/release/v1.10.2/bin/darwin/amd64/kubectl ;;
+        linux*)   wget https://storage.googleapis.com/kubernetes-release/release/v1.10.2/bin/linux/amd64/kubectl;;
+        *)        echo "Unsupported operating system: $OSTYPE" ;;
+    esac
+
+    chmod +x kubectl
+
+    # install in the expected path
+    sudo mv kubectl /usr/local/bin/
+}
+
+# download and install Helm CLI tool
+function install_helm {
+    echo "Installing 'helm' CLI"
+
+    # download and extract the binary for the current env
+    case "$OSTYPE" in
+        darwin*)  curl https://kubernetes-helm.storage.googleapis.com/helm-v2.8.1-darwin-amd64.tar.gz | tar xvz --strip-components=1 darwin-amd64/helm ;;
+        linux*)   curl https://kubernetes-helm.storage.googleapis.com/helm-v2.8.1-linux-amd64.tar.gz | tar xvz --strip-components=1 linux-amd64/helm ;;
+        *)        echo "Unsupported operating system: $OSTYPE" ;;
+    esac
+
+    chmod +x helm
+    sudo mv helm /usr/local/bin/
+}
+
+# download and install 'svcat' CLI
+function install_svcat(){
+   echo "Installing 'svcat' CLI"
+
+   # download and extract the binary for the current env
+   case "$OSTYPE" in
+        darwin*)  curl -sLO https://download.svcat.sh/cli/latest/darwin/amd64/svcat ;;
+        linux*)   curl -sLO https://download.svcat.sh/cli/latest/linux/amd64/svcat  ;;
+        *)        echo "Unsupported operating system: $OSTYPE" ;;
+    esac
+
+    chmod +x ./svcat
+    mv ./svcat /usr/local/bin/
+}
+
+# extract and setup environmental variables
+function setup_env_vars(){
+
+    # check if OSBA stability has not been set
+   if [[ ! ${OSBA_STABILITY+x} ]]; then
+     # if not set it sets default to experimental
+     OSBA_STABILITY="EXPERIMENTAL"
+   fi
+
+    # extract subscription id
+   AZURE_SUBSCRIPTION_ID="$(az account show --query id --out tsv)"
+    # create service principal and store its JSON output
+   SERVICE_PRINCIPAL_JSON="$(az ad sp create-for-rbac --name osba-sp -o json)"
+    # extract svc principal name from svc-principal creation output
+   AZURE_SP_NAME="$(echo $SERVICE_PRINCIPAL_JSON | jq -r .name)"
+    # extract tenant-id from svc-principal creation output
+   AZURE_TENANT_ID="$(echo $SERVICE_PRINCIPAL_JSON | jq -r .tenant)"
+    # extract client-id from svc-principal creation output
+   AZURE_CLIENT_ID="$(echo $SERVICE_PRINCIPAL_JSON | jq -r .appId)"
+    # extract client-secret from svc-principal creation output
+   AZURE_CLIENT_SECRET="$(echo $SERVICE_PRINCIPAL_JSON | jq -r .password)"
+}
+
+function remove_osba_installation(){
+
+      # disable exit on non 0
+      set +e
+
+      # remove the service-catalog deployment
+      echo "Removing service-catalog deployment"
+      helm delete catalog --purge
+
+      # remove the OSBA deployment
+       echo "Removing OSBA deployment"
+      helm delete osba --purge
+      helm delete osba-quickstart --purge
+      kubectl delete namespace osba
+
+      # delete the service principlan using the
+      # predefined name i.d 'osba-sp'
+      echo "Removing service principal"
+      az ad sp delete --id http://osba-sp
+}
+
+# parse arguments
+while (( "$#" )); do
+    case "$1" in
+    -h|--help)
+      echo "osba.sh : OSBA Installer for ACS-Engine Kubernetes deployments."
+      echo "Usage: ./osba.sh [options]"
+      echo "options: "
+      echo "    -r, --remove                    Remove OSBA installation."
+      echo "    -h, --help                      Display this help message."
+      exit 0
+      ;;
+    -r | --remove)
+      echo "- Removing OSBA deployment"
+      remove_osba_installation
+      exit 0
+      ;;
+    \? )
+      echo "Invalid Option: -$OPTARG" 1>&2
+      exit 1
+      ;;
+  esac
+done
+
+echo "- Starting OSBA deployment..."
+
+###
+# extract and setup environmental variables
+setup_env_vars
+
+##
+# Check version requirements
+
+# verify kubectl exists
+if ! [ -x "$(command -v kubectl)" ]; then
+  echo 'Error: kubectl is not installed.' >&2
+  install_kubectl
+fi
+
+# kubectl version check
+KUBECLT_MAJOR_VERSION=$(kubectl version --insecure-skip-tls-verify=true --client -o json | jq  -r .clientVersion.major)
+if [ $KUBECLT_MAJOR_VERSION != 1 ]; then
+  echo "* Kubectl major version != 1 ... \n * installing compatible version ..."
+  install_kubectl
+fi
+
+KUBECLT_MINOR_VERSION=$(kubectl version --insecure-skip-tls-verify=true --client -o json | jq  -r .clientVersion.minor)
+if [ $KUBECLT_MINOR_VERSION -lt 9 ]; then
+  echo "Kubectl minor version < 9 ...  \n * installing compatible version ..."
+  install_kubectl
+fi
+
+# Verify Helm CLI exists
+if ! [ -x "$(command -v helm)" ]; then
+  echo 'Error: helm is not installed.' >&2
+  install_helm
+fi
+
+# upgrade the Tiller component
+helm init \
+  --force-upgrade
+
+# initialise Helm in client-only mode
+helm init \
+  --client-only
+
+# setup service-catalog Helm repository
+helm repo add svc-cat https://svc-catalog-charts.storage.googleapis.com
+
+# update Helm repository
+helm repo update
+
+# install Service Catalog
+# (https://svc-cat.io/docs/install)
+helm install svc-cat/catalog \
+    --name catalog \
+    --namespace catalog \
+    --set rbacEnable=true \
+    --set apiserver.storage.etcd.persistence.enabled=true \
+    --wait
+
+# wait untill the deployment of svc-cat/catalog complete
+kubectl rollout status deploy/catalog-catalog-apiserver --namespace=catalog -w
+kubectl rollout status deploy/catalog-catalog-controller-manager --namespace=catalog -w
+
+# verify 'svcat' CLI exists
+if ! [ -x "$(command -v svcat)" ]; then
+  echo 'Error: svcat is not installed.' >&2
+  install_svcat
+fi
+
+# add azure repo in helm
+helm repo add azure https://kubernetescharts.blob.core.windows.net/azure
+
+#  deploy osba chart
+helm install azure/open-service-broker-azure \
+  --name osba \
+  --namespace osba \
+  --set azure.subscriptionId=$AZURE_SUBSCRIPTION_ID \
+  --set azure.tenantId=$AZURE_TENANT_ID \
+  --set azure.clientId=$AZURE_CLIENT_ID \
+  --set azure.clientSecret=$AZURE_CLIENT_SECRET \
+  --set modules.minStability=$OSBA_STABILITY \
+  --wait
+
+# wait untill the deployment of azure/open-service-broker-azure complete
+kubectl rollout status deploy/osba-open-service-broker-azure --namespace=osba -w
+kubectl rollout status deploy/osba-redis --namespace=osba -w
+
+echo "OSBA deployment completed! ... Enjoy!"
